9a11310bccb6d7afae4ebad0e34ffb0700378d5d
VaDick <krem102@gmail.com>
2018-10-19 02:09:28 +0300

    dk,sfdsbnfvnmxdvm,nxs

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const server = http.createServer(function(request, response) {
     }
 });
 
-console.log(' port = ', process.env.PORT );
+console.log('port = ', process.env.PORT );
 
 server.listen(process.env.PORT || 3000);
 console.log('Server started!');
+/////////////--------------------
